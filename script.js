0789a675700f06c752f77f6a2cc979f1fd74b4e8
Sameer080706 <samasameer124@gmail.com>
2025-06-26 12:21:42 +0530

    Minor changes

--- script.js
+++ script.js
@@ -8,65 +8,78 @@ const random = document.getElementById("random");
 const loader = document.getElementById("loader");
 
 const addLoad = () => {
-    loader.classList.add("loader");
+  loader.classList.add("loader");
 };
 const removeLoad = () => {
-    loader.classList.remove("loader");
+  loader.classList.remove("loader");
 };
+
+container.addEventListener("click", (e)=> {
+  if (e.target.tagName == "IMG") {
+    console.log("Image is clicked")
+    window.open(e.target.src)
+  }
+  else{
+   console.log("Image is not clicked") 
+  }
+})
 // display Data
 const display = data => {
-    image = document.createElement("img");
-    image.src = data;
-    image.loading = "lazy";
-    container.prepend(image);
+  image = document.createElement("img");
+  image.src = data;
+  image.loading = "lazy";
+  container.prepend(image);
 };
 // Get Data
 const getData = async name => {
-    addLoad();
-    try {
-        if (name == "") {
-          removeLoad();
-            alert("Please Enter Pokemon Name! Or click Random!");
-        } else {
-            const respons = await fetch(
-                `https://api.pokemontcg.io/v2/cards?q=name:${name}`
-            );
-            let data = await respons.json();
-            data = data.data;
-            console.log(data);
-            if (data.length == 0) {
-                removeLoad();
-                alert("Data Not Found!");
-            } else {
-                removeLoad();
-                for (let i = 0; i < data.length; i++) {
-                    display(data[i].images.large);
-                }
-            }
+  addLoad();
+  try {
+    if (name == "") {
+      removeLoad();
+      alert("Please Enter Pokemon Name! Or click Random!");
+    } else {
+      const respons = await fetch(
+        `https://api.pokemontcg.io/v2/cards?q=name:${name}`
+      );
+      let data = await respons.json();
+      data = data.data;
+
+      if (data.length == 0) {
+        removeLoad();
+        alert("Data Not Found!");
+      } else {
+        removeLoad();
+        for (let i = 0; i < data.length; i++) {
+          display(data[i].images.large);
         }
-    } catch (e) {
-        console.log(e);
+      }
     }
+  } catch (e) {
+    removeLoad();
+    alert("Please try again")
+  }
 };
 
 // assemble function
 btn.addEventListener("click", () => {
-    event.preventDefault();
-    let name = search.value.toLowerCase();
-    getData(name);
+  event.preventDefault();
+  let name = search.value.toLowerCase();
+  getData(name);
 });
 
 const getRandom = async () => {
-    event.preventDefault();
-    addLoad();
-    let num = Math.floor(Math.random() * 1302);
-    try {
-        const respons = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
-        let data = await respons.json();
-        removeLoad();
-        getData(data.name);
-    } catch (e) {
-        console.log(e);
-    }
+  event.preventDefault();
+  addLoad();
+  let num = Math.floor(Math.random() * 1302);
+  try {
+    const respons = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
+    let data = await respons.json();
+    removeLoad();
+    getData(data.name);
+  } catch (e) {
+    console.log(e);
+    removeLoad();
+    alert("Please try again")
+  }
 };
 random.addEventListener("click", getRandom);
